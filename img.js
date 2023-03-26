361d072e85727f786c14f8c26a44224d5e628195
Pinapplekat <elijah.ryerson@gmail.com>
2023-03-26 14:53:46 -0700

    Added

--- img.js
+++ img.js
@@ -7,20 +7,25 @@ function getByteArray(filePath) {
     result.push("0x" + fileData[i] + "" + fileData[i + 1]);
   return result;
 }
-
+var page
+var browser
 const express = require("express");
 const puppeteer = require("puppeteer");
 const app = express();
 const port = 3000;
-var browser_sessions = [];
-app.get("/url", async (req, res) => {
+async function start() {
+  browser = await puppeteer.launch();
+  page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 1080 });
+  await page.goto('https://google.com/', { waitUntil: "networkidle0" });
+}
+start()
+app.get("/url.jpg", async (req, res) => {
   console.log("REQUEST RECIEVED");
   var { url } = req.query;
-  url = 'https://'+url
+  url = 'https://' + url
   console.log(url)
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.setViewport({ width: 1920, height: 1080 });
+  await page.setViewport({ width: 2560, height: 1440 });
   await page.goto(url, { waitUntil: "networkidle0" });
   var scrnsht = await page.screenshot({
     path: "screenshot.jpg",
@@ -30,8 +35,64 @@ app.get("/url", async (req, res) => {
   res.sendFile(__dirname + "/screenshot.jpg");
   // res.json(byteFile)
   console.log("sent");
-  browser.close();
+  // browser.close();
 });
+app.get("/key.jpg", async (req, res) => {
+  var { keycode } = req.query
+  await page.keyboard.press(keycode, { waitUntil: "networkidle0" })
+  var scrnsht = await page.screenshot({
+    path: "screenshot.jpg",
+  });
+
+  const byteFile = getByteArray(__dirname + "/screenshot.jpg");
+  res.sendFile(__dirname + "/screenshot.jpg");
+})
+app.get("/type.jpg", async (req, res) => {
+  var { text } = req.query
+  await page.keyboard.type(text, { waitUntil: "networkidle0" })
+  var scrnsht = await page.screenshot({
+    path: "screenshot.jpg",
+  });
+
+  const byteFile = getByteArray(__dirname + "/screenshot.jpg");
+  res.sendFile(__dirname + "/screenshot.jpg");
+})
+app.get("/keybind.jpg", async (req, res) => {
+  var { key, shift, control } = req.query
+  console.log(shift,control)
+  if (shift == "true") {
+    await page.keyboard.down("ShiftLeft")
+    console.log("SHIFT")
+  }
+  if (control == "true") {
+    await page.keyboard.down("ControlLeft")
+    console.log("CTRL")
+  }
+  await page.keyboard.press(`Key${key.toUpperCase()}`)
+  if (shift == "true") {
+    await page.keyboard.up("ShiftLeft")
+  }
+  if (control == "true") {
+    await page.keyboard.up("ControlLeft")
+  }
+  var scrnsht = await page.screenshot({
+    path: "screenshot.jpg",
+  });
+
+  const byteFile = getByteArray(__dirname + "/screenshot.jpg");
+  res.sendFile(__dirname + "/screenshot.jpg");
+})
+app.get("/screenshot.jpg", async (req, res) => {
+  var scrnsht = await page.screenshot({
+    path: "screenshot.jpg",
+  });
+
+  const byteFile = getByteArray(__dirname + "/screenshot.jpg");
+  res.sendFile(__dirname + "/screenshot.jpg");
+})
+app.get("/close", (req, res) => {
+  browser.close()
+})
 app.get("/test", (req, res) => {
   res.send("TEST");
 });
